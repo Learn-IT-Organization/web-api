f54b2a2d7560ad4f747268b949b942e14f6ba151
DemeterEvelin14 <99736246+DemeterEvelin14@users.noreply.github.com>
2023-11-04 13:49:26 +0200

    #13: Chapters-related API calls implemented

--- src/app.js
+++ src/app.js
@@ -3,15 +3,16 @@ import pkg from '../config/database.cjs';
 const { sequelize } = pkg; 
 import courseRoutes from './routes/courseRoutes.js'; 
 import userRoutes from './routes/userRoutes.js';
+import chapterRoutes from './routes/chapterRoutes.js';
 
 const app = express();
 const port = 3306;
 
 app.use(express.json());
-app.use(express.json());
 
 app.use('/', courseRoutes);
 app.use('/', userRoutes);
+app.use('/', chapterRoutes);
 
 sequelize.sync = async () => {
   await sequelize.sync();
