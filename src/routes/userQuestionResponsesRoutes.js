2e9fb92bd60c961cdaff49c07b76795ce4391411
DemeterEvelin14 <99736246+DemeterEvelin14@users.noreply.github.com>
2024-02-14 18:06:43 +0200

    #99: Sequential lesson completion

--- src/routes/userQuestionResponsesRoutes.js
+++ src/routes/userQuestionResponsesRoutes.js
@@ -3,8 +3,11 @@ import {
   respond,
   getResponsesByUser,
   getAllResponses,
+  getLessonResult,
+  deleteUserResponsesByLesson
 } from "../controllers/userQuestionResponseController.js";
 import { validateStudent } from "../middleware/roleMiddleware.js";
+import { validateToken } from "../middleware/JWT.js";
 
 const router = Router();
 
@@ -15,5 +18,9 @@ router.post("/respond", validateStudent, respond);
 router.get("/responses", validateStudent, getAllResponses);
 
 router.get("/userResponses/:id", validateStudent, getResponsesByUser);
+
+router.get("/lessonResult/:lessonId", validateToken, getLessonResult);
+
+router.get("/deleteResponses/:lessonId", validateToken, deleteUserResponsesByLesson);
 
 export default router;
