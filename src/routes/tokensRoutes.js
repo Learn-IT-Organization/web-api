48a0f0b1f645951626c0fa722c81e17d5c93912e
KrisztaOniga <99740799+KrisztaOniga@users.noreply.github.com>
2024-03-08 09:27:41 +0200

    bug fixed

--- src/routes/tokensRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendToken } from "../controllers/tokensController.js";
-
-const router = Router();
-
-router.post("/sendToken", sendToken);
-
-export default router;
