b64ba9ff74099cc459a31795d8816ec3cfb5713f
DemeterEvelin14 <99736246+DemeterEvelin14@users.noreply.github.com>
2024-05-28 09:58:39 +0300

    bug fixed

--- src/controllers/teacherRequestController.js
+++ src/controllers/teacherRequestController.js
@@ -4,39 +4,37 @@ import HTTP_STATUS_CODES from "../constants/httpStatusCodes.js";
 
 const validateTeacherRequest = async (req, res, next) => {
   const { request_id, user_id } = req.body;
+  const teacherRequest = await TeacherRequest.findByPk(request_id);
 
-  try {
-    const teacherRequest = await TeacherRequest.findByPk(request_id);
-
-    if (!teacherRequest) {
-      return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: "Teacher request not found" });
-    }
-
-    if (teacherRequest.is_approved === "accepted") {
-      return res
-        .status(HTTP_STATUS_CODES.NOT_FOUND)
-        .json({ message: "Teacher request is already approved" });
-    }
-
-    teacherRequest.is_approved = "accepted";
-    await teacherRequest.save();
-
-    const user = await Users.findByPk(user_id);
-
-    if (!user) {
-      return res.status(HTTP_STATUS_CODES.CREATED.NOT_FOUND).json({ message: "User not found" });
-    }
-
-    user.user_role = "teacher";
-    await user.save();
-
-    res
-      .status(HTTP_STATUS_CODES.OK)
-      .json({ message: "Teacher request approved and user role updated" });
-  } catch (error) {
-    console.error("Error validating teacher request:", error);
-    res.status(INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
+  if (!teacherRequest) {
+    return res
+      .status(HTTP_STATUS_CODES.NOT_FOUND)
+      .json({ message: "Teacher request not found" });
   }
+
+  if (teacherRequest.is_approved === "accepted") {
+    return res
+      .status(HTTP_STATUS_CODES.NOT_FOUND)
+      .json({ message: "Teacher request is already approved" });
+  }
+
+  teacherRequest.is_approved = "accepted";
+  await teacherRequest.save();
+
+  const user = await Users.findByPk(user_id);
+
+  if (!user) {
+    return res
+      .status(HTTP_STATUS_CODES.CREATED.NOT_FOUND)
+      .json({ message: "User not found" });
+  }
+
+  user.user_role = "teacher";
+  await user.save();
+
+  res
+    .status(HTTP_STATUS_CODES.OK)
+    .json({ message: "Teacher request approved and user role updated" });
 };
 
 const createRequest = async (req, res) => {
@@ -53,4 +51,4 @@ const getAllRequests = async (req, res) => {
   res.status(HTTP_STATUS_CODES.OK).json(requests);
 };
 
-export { validateTeacherRequest , createRequest, getAllRequests};
+export { validateTeacherRequest, createRequest, getAllRequests };
