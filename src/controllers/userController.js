0c1218b5b8b4e0ce0c4ccb8e4e037f891eec407f
KrisztaOniga <99740799+KrisztaOniga@users.noreply.github.com>
2024-06-16 10:00:52 +0300

    #147: Routes fixed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,138 @@
 import Users from "../models/userModel.js";
 import HTTP_STATUS_CODES from "../constants/httpStatusCodes.js";
+import bcrypt from "bcrypt";
 import { RecordNotFoundError } from "../constants/errors.js";
-import { validateToken } from "../middleware/JWT.js";
+import { validateToken, createTokens } from "../middleware/JWT.js";
+
+const login = async (req, res) => {
+  const { user_name, user_password } = req.body;
+  const user = await Users.findOne({ where: { user_name: user_name } });
+
+  if (!user) {
+    const errorResponse = {
+      success: false,
+      data: null,
+      err: {
+        code: "SERVICE_CODE",
+        msg: "ERR_USER_NOT_FOUND",
+      },
+      servertime: Date.now(),
+    };
+    res.status(HTTP_STATUS_CODES.NOT_FOUND).json(errorResponse);
+    return; 
+  }
+
+  const dbPassword = user.user_password;
+
+  if (!dbPassword) {
+    const errorResponse = {
+      success: false,
+      data: null,
+      err: {
+        code: "SERVICE_CODE",
+        msg: "ERR_INVALID_PASSWORD",
+      },
+      servertime: Date.now(),
+    };
+    res.status(HTTP_STATUS_CODES.BAD_REQUEST).json(errorResponse);
+    return; 
+  }
+
+  bcrypt.compare(user_password, dbPassword).then((match) => {
+    if (!match) {
+      const errorResponse = {
+        success: false,
+        data: null,
+        err: {
+          code: "SERVICE_CODE",
+          msg: "ERR_INVALID_PASSWORD",
+        },
+        servertime: Date.now(),
+      };
+      res.status(HTTP_STATUS_CODES.BAD_REQUEST).json(errorResponse);
+    } else {
+      const { accessToken, expiresAt } = createTokens(user, req);
+
+      res.cookie("access-token", accessToken, {
+        maxAge: 60 * 60 * 24 * 30 * 1000,
+        httpOnly: false,
+        secure: false,
+      });
+
+      const successResponse = {
+        success: true,
+        data: {
+          name: "token",
+          value: accessToken,
+          expires: expiresAt,
+        },
+        err: null,
+        message: "Logged in successfully",
+        servertime: Date.now(),
+      };
+      res.json(successResponse);
+    }
+  });
+};
+
+const logout = (req, res) => {
+  try {
+    res.clearCookie("access-token");
+
+    res.json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    console.error("Logout error:", error);
+    if (!res.headersSent) {
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ success: false, error: "Internal Server Error" });
+    }
+  }
+};
+
+const register = async (req, res) => {
+  const {
+    user_role,
+    first_name,
+    last_name,
+    user_name,
+    user_password,
+    gender,
+    user_level,
+    streak,
+  } = req.body;
+
+  try {
+    const existingUser = await Users.findOne({
+      where: { user_name: user_name },
+    });
+
+    if (existingUser) {
+      return res
+        .status(HTTP_STATUS_CODES.BAD_REQUEST)
+        .json({ success: false, error: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(user_password, 10);
+
+    await Users.create({
+      user_role: user_role,
+      first_name: first_name,
+      last_name: last_name,
+      user_name: user_name,
+      user_password: hashedPassword,
+      gender: gender,
+      user_level: user_level,
+      streak: streak,
+    });
+
+    res.json({ success: true, message: "User registered" });
+  } catch (err) {
+    res
+      .status(HTTP_STATUS_CODES.BAD_REQUEST)
+      .json({ error: err.message || "Error registering user" });
+  }
+};
 
 const createUser = async (req, res) => {
   try {
@@ -29,4 +160,4 @@ const getUserById = async (req, res) => {
   }
 };
 
-export { createUser, getAllUsers, getUserById };
+export { login, logout, register, createUser, getAllUsers, getUserById };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import {
+  login,
+  logout,
+  register,
   createUser,
   getAllUsers,
   getUserById,
 } from "../controllers/userController.js";
 
 const router = Router();
+
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/register", register);
 
 router.post("/users", createUser);
 router.get("/users", getAllUsers);
